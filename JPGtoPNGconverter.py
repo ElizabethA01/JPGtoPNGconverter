989e264143bd743b915b81e10c6876253ab2ab89
Elizabeth Adejumo <a.adejumo13@gmail.com>
2021-10-24 16:31:11 +0100

    shortened code

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -5,29 +5,24 @@ from PIL import Image
 #grab first and second argument
 image_folder = sys.argv[1]
 output_folder = sys.argv[2]
-# print(image_folder)
-# print(output_folder)
-
-new_path = os.path.join(image_folder, output_folder)
-print(new_path)
 
 #check if new/ exists, if not create
 try:
-    if not os.path.exists('.\Pokedex\new'):
-        os.makedirs(new_path)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 except FileExistsError as err:
     print('File already exist')
 
 # loop through Pokedex
 for filename in os.listdir(image_folder):
     if filename.endswith('.jpg'):
-        img = Image.open(os.path.join(image_folder, filename))
+        img = Image.open(f'{image_folder}{filename}')
         #convert images to png
-        name =  filename + '.png'
-        img.save(name)
+        clean_name = os.path.splitext(filename)[0]
         #save to the new folder
-        complete_name = os.path.join(new_path, filename)
-        print(complete_name)
+        img.save(f'{output_folder}{clean_name}.png','png')
+        #save to the new folder
+        # complete_name = os.path.join(new_path, filename)
         continue
     else:
         continue
